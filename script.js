744b5e3971a3f7dfff4adeef6348123e103b5071
unknown <michail.kem@yandex.ru>
2022-05-14 02:08:00 +0300

    аучился использовать цилы

--- script.js
+++ script.js
@@ -1,19 +1,47 @@
-const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
 
-const personalMovieDB = {
-  count: numberOfFilms,
-  movies: {},
-  actors: {},
-  genres: [],
-  privat: false
-};
+// let i = 4;
 
-const a = prompt('Один из последних просмотренных фильмов?', ''),
-      b = prompt('На сколько оцените его?', ''),
-      c = prompt('Один из последних просмотренных фильмов', ''),
-      d = prompt('На сколько оцените его?', '');
+// while (i < 11) {
+//   i++;
+//   console.log(i);
+// }
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+// for (let j = 20; j >= 10; j--) {
+//   if (j === 13) break;
+//   console.log(j);
+// }
 
-console.log(personalMovieDB);
+// for (let y = 2; y <=10; y++) {
+//   if (y % 2 === 0) {
+//     console.log(y);
+//   }
+// }
+
+// let a = 2;
+
+// while (a <= 16) {
+//   if (a % 2 === 0){
+//     a++;
+//     continue;
+//   } else {
+//     console.log(a);
+//   }
+//   a++;
+// }
+
+const lines = 5;
+let result = '';
+
+for (let i = 0; i <= lines; i++) {
+  for (let j = 0; j < lines - i; j++) {
+    result += " ";
+  }
+  for (let j = 0; j < 2 * i + 1; j++) {
+    result += "*";
+  }
+  result += "\n"
+}
+
+console.log(result)
+
+
