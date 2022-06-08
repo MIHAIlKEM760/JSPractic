86dec84675855eebd738dbe14e3d661dac4d0231
unknown <michail.kem@yandex.ru>
2022-06-08 20:00:07 +0300

    создал функцию start

--- 1/script.js
+++ 1/script.js
@@ -1,4 +1,15 @@
-const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+'use strict';
+
+let numberOfFilms;
+
+function () {
+  numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+
+  while (numberOfFilms == '', numberOfFilms == null, isNaN(numberOfFilms) ) {
+    numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+  }
+}
+
 
 const personalMovieDB = {
   count: numberOfFilms,
@@ -8,18 +19,36 @@ const personalMovieDB = {
   privat: false
 };
 
-for (let i = 0; i < 2; i++) {
-  const a = prompt('Один из последних просмотренных фильмов?', ''),
-      b = prompt('На сколько оцените его?', '');
-
-      if (a != null && b != null && a != '' && b != '' && a.length < 50)  {
-        personalMovieDB.movies[a] = b;
-        console.log('done');
-      } else {
-        console.log('error');
-        i--;
-      }
+function rememberMyFilms() {
+  for (let i = 0; i < 2; i++) {
+    const a = prompt('Один из последних просмотренных фильмов?', ''),
+        b = prompt('На сколько оцените его?', '');
+  
+        if (a != null && b != null && a != '' && b != '' && a.length < 50)  {
+          personalMovieDB.movies[a] = b;
+          console.log('done');
+        } else {
+          console.log('error');
+          i--;
+        }
+  }
 }
+
+rememberMyFilms();
+
+function detectPersonalLevel() {
+  if (personalMovieDB.count < 10) {
+    console.log("Посмотрел мало фильмов");
+  } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+    console.log("Вы классический зритель");
+  } else if (personalMovieDB.count >= 30) {
+    console.log("Вы киноман");
+  } else {
+    console.log("Произошла ошибка");
+  }
+}
+
+detectPersonalLevel();
 
 if (personalMovieDB.count < 10) {
   console.log("Посмотрел мало фильмов");
--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+"use strict";
 
 // let i = 4;
 
@@ -29,19 +30,127 @@
 //   a++;
 // }
 
-const lines = 5;
-let result = '';
+// const lines = 5;
+// let result = '';
 
-for (let i = 0; i <= lines; i++) {
-  for (let j = 0; j < lines - i; j++) {
-    result += " ";
-  }
-  for (let j = 0; j < 2 * i + 1; j++) {
-    result += "*";
-  }
-  result += "\n"
-}
+// for (let i = 0; i <= lines; i++) {
+//   for (let j = 0; j < lines - i; j++) {
+//     result += " ";
+//   }
+//   for (let j = 0; j < 2 * i + 1; j++) {
+//     result += "*";
+//   }
+//   result += "\n"
+// }
 
-console.log(result)
+// console.log(result)
+
+// for (let a = 5; a <= 10; a++) {
+//   console.log(a)
+// }
+
+// let a = 5;
+// while (a <=10) {
+//   console.log(a);
+//   a++
+// }
+
+// let i = 2;
+// while (i <= 16) {
+  
+//   if (i % 2 ===0) {
+//     i++;
+//     continue;
+//   } else {
+//     console.log(i);
+//   }
+//   i++;
+// }
+
+// function firstTask() {
+//   const arr = [3, 5, 8, 16, 20, 23, 50];
+//   const result = [];
+
+//   for (let i = 0; i < arr.length; i++) {
+//       result[i] = arr[i];
+//   }
+
+//   console.log(result);
+//   return result;
+// }
+
+// firstTask();
 
 
+
+// function returnNeighboringNumbers(num) {
+//   return [num - 1, num, num + 1];
+// }
+
+// returnNeighboringNumbers(5);
+
+// function getMathResult(num, times) {
+//   if (typeof(times) !== 'number' || times <= 0) {
+//       return num;
+//   }
+
+//   let str = '';
+
+//   for (let i = 1; i <= times; i++) {
+//       if (i === times) {
+//           str += `${num * i}`;
+//           // Тут без черточек в конце
+//       } else {
+//           str += `${num * i}---`;
+//           // Это тоже самое, что и
+//           // str = str + num * i + "---"
+//       }
+//   }
+
+//   return str;
+// }
+
+// getMathResult(10, 5);
+
+// let nam, admin;
+
+// nam = "джон";
+
+// admin = nam ;
+
+// alert(admin);
+
+// let ECMAScript = prompt('Какое "официальное" название JavaScript', '');
+
+// if (ECMAScript == 'ECMAScript') {
+//   alert('Верно');
+// } else {
+//   alert('ECMAScript');
+// }
+
+// let numbers = prompt('Введтье число', '');
+
+// if (numbers > 0) {
+//   alert(1);
+// } else if (numbers < 0) {
+//   alert(-1);
+// } else {
+//   alert(0);
+// }
+
+// let result = (a + b < 4) ? 'Мало' : 'Много';
+
+let message = (login == 'Сотрудник') ? 'Привет' :
+  (login == 'Директор') ? 'Здравствуйте' :
+  (login == '') ? 'Нет логина':
+  '';
+
+// if (login == 'Сотрудник') {
+//   message = 'Привет';
+// } else if (login == 'Директор') {
+//   message = 'Здравствуйте';
+// } else if (login == '') {
+//   message = 'Нет логина';
+// } else {
+//   message = '';
+// }
